b9ee08f16cef58a350e6c72040dee6d8ae6769eb
scribeNcode <deyeye99@gmail.com>
2024-12-27 03:11:55 +0100

    add playRound function to console.log a string value representing the round winner

--- script.js
+++ script.js
@@ -8,17 +8,16 @@ function getComputerChoice(){
     // Randomly return this : “rock”, “paper” or “scissors”
     switch (randomNum) {
         case 0 :
-            return "rock"
             console.log("rock")
+            return "rock"
             break;
 
         case 1 :
-            return "paper"
             console.log("paper")
+            return "paper"
             break;
         default:
             return "scissors"
-            console.log("Scissors")
             break;
     }
 }
@@ -30,9 +29,11 @@ function getComputerChoice(){
 // Write the logic to get the human choice
 
 function getHumanChoice(){
-    let userInput = prompt('');
+    let userInput = prompt('').toLowerCase();
     return userInput
-    console.log(userInput)
+    
+    
+    
 }
 
 // getHumanChoice()
@@ -46,3 +47,40 @@ let computerScore = 0;
 
 
 
+
+// A function to console log the result of  round
+
+// create a variable that would be passed as parameter
+let human = getHumanChoice()
+
+let computer = getComputerChoice()
+//  console.log(`This is ${human}, This is ${computer}`)
+
+
+
+function playRound( computerChoice , humanChoice,){
+    if(computerChoice === "rock" && humanChoice === "scissors"){
+        result = `You lose! ${computerChoice} beats ${humanChoice}.`
+        console.log(result)
+        return result
+    }else if(computerChoice === "scissors" && humanChoice === "paper"){
+        result = `You lose! ${computerChoice} beats ${humanChoice}.`
+        console.log(result)
+        return result 
+    }else if(computerChoice === "paper" && humanChoice === "rock"){
+        result = `You lose! ${computerChoice} beats ${humanChoice}.`
+        console.log(result)
+        return result 
+    }else if(computerChoice === humanChoice){
+        result = `It's a tie`
+        console.log(result)
+        return result
+    }else{
+        result = `You won ! ${humanChoice} beats ${computerChoice}`
+        console.log(result)
+        return result
+    }
+
+}
+
+playRound(computer, human);
